73f4293ae0f8ca3fc18485746123663e36f0fbc6
maljaroudi1 <meojax.ajx@gmail.com>
2023-09-27 18:53:01 -0400

    new files

--- src/assets/index.js
+++ src/assets/index.js
@@ -27,7 +27,7 @@ import docker from './tech/docker.png';
 import figma from './tech/figma.png';
 import git from './tech/git.png';
 import html from './tech/html.png';
-import javascript from './tech/javascript.png';
+import javascript from './tech/Javascript.png';
 import nodejs from './tech/nodejs.png';
 import reactjs from './tech/reactjs.png';
 import redux from './tech/redux.png';
