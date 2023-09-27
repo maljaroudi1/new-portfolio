4fb123570c8181034da24428f0124f20fe745444
maljaroudi1 <meojax.ajx@gmail.com>
2023-09-27 18:40:45 -0400

    new files

---
+++ src/components/About.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+import '../styles/about/about.css'
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        Welcome to my portfolio, a showcase of my journey as a Full Stack React.js Developer.
+        My passion lies in creating immersive web experiences that seamlessly blend aesthetics and functionality.
+        With my expertise in React.js, I specialize in designing and developing interactive front-end interfaces integrated with robust server-side solutions.
+        My diverse skill set spans HTML, SCSS, ReactJS, Node.js, MongoDB, ExpressJS, NextJS and Firebase, enabling me to take projects from concept to deployment.
+        These projects serve as a testament to my technical prowess and creativity, illustrating my ability to excel in both front-end and back-end development.
+         Explore my portfolio to witness the depth of my capabilities and dedication to crafting remarkable web solutions
+      </motion.p>
+
+      <div className="box mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
---
+++ src/constants/index.js
@@ -0,0 +1,264 @@
+import sass from '../assets/tech/sass.png'
+import mongodb from '../assets/tech/mongodb.png'
+import firebase from '../assets/tech/firebase.png'
+import wordpress from '../assets/tech/wordpress.png'
+import carrental from '../assets/projects/car-rental.png'
+import bluelight from '../assets/company/bluelight.webp'
+import upwork from '../assets/company/upwork.png'
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  graphql,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  {
+    title: 'UI/UX Design',
+    icon: ux,
+  },
+  {
+    title: 'Software Prototyping',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'sass',
+    icon: sass,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'mongodb',
+    icon: mongodb,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'wordpress',
+    icon: wordpress,
+  },
+  {
+    name: 'firebase',
+    icon: firebase,
+  },
+];
+
+const experiences = [
+  {
+    title: 'React Engineer - Remote, Full-Time',
+    company_name: 'Bluelight Consulting',
+    icon: bluelight,
+    iconBg: '#333333',
+    date: 'march 2022 - present',
+  },
+  {
+    title: 'Freelancer',
+    company_name: 'Upwork',
+    icon: upwork,
+    iconBg: '#333333',
+    date: 'Sep 2023 - present',
+  },
+  // {
+  //   title: 'Junior Software Engineer',
+  //   company_name: 'Kelhel',
+  //   icon: kelhel,
+  //   iconBg: '#333333',
+  //   date: 'May 2022 - Oct 2022',
+  // },
+  // {
+  //   title: 'Full Stack Developer',
+  //   company_name: 'Diversity Cyber Council',
+  //   icon: dcc,
+  //   iconBg: '#333333',
+  //   date: 'Sep 2022 - Present',
+  // },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'RideGO Car Rental',
+    description: 'A fully comprehensive car rental website equipped with advanced functionalities, including vehicle categorization, robust search capabilities, seamless booking processes, user authentication, user dashboard and even a chatbot.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: carrental,
+    repo: 'https://github.com/maljaroudi1/car-rental-rentgo.git',
+    demo: 'https://car-rental-refined-3jl464kas-maljaroudi2000.vercel.app/',
+  },
+  {
+    id: 'project-2',
+    name: 'Leaderboard',
+    description:
+      'A leaderboard list app that displays scores submitted by different players.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: carrental,
+    repo: 'https://github.com/shaqdeff/Leaderboard',
+    demo: 'https://shaqdeff.github.io/Leaderboard/',
+  }
+  // {
+  //   id: 'project-3',
+  //   name: 'Math Magicians',
+  //   description: 'This is a single-page calculator app built with React',
+  //   tags: [
+  //     {
+  //       name: 'nextjs',
+  //       color: 'blue-text-gradient',
+  //     },
+  //     {
+  //       name: 'supabase',
+  //       color: 'green-text-gradient',
+  //     },
+  //     {
+  //       name: 'css',
+  //       color: 'pink-text-gradient',
+  //     },
+  //   ],
+  //   image: math,
+  //   repo: 'https://github.com/shaqdeff/Math-Magicians',
+  //   demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+  // },
+  // {
+  //   id: 'project-4',
+  //   name: 'Movie Metro',
+  //   description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+  //   tags: [
+  //     {
+  //       name: 'nextjs',
+  //       color: 'blue-text-gradient',
+  //     },
+  //     {
+  //       name: 'supabase',
+  //       color: 'green-text-gradient',
+  //     },
+  //     {
+  //       name: 'css',
+  //       color: 'pink-text-gradient',
+  //     },
+  //   ],
+  //   image: movie,
+  //   repo: 'https://github.com/shaqdeff/Movie-Metro',
+  //   demo: 'https://movie-metro.netlify.app/',
+  // },
+  // {
+  //   id: 'project-5',
+  //   name: 'Nyeusi Fest Site',
+  //   description:
+  //     'This is a demo concert website for a music festival called Nyeusi.',
+  //   tags: [
+  //     {
+  //       name: 'nextjs',
+  //       color: 'blue-text-gradient',
+  //     },
+  //     {
+  //       name: 'supabase',
+  //       color: 'green-text-gradient',
+  //     },
+  //     {
+  //       name: 'css',
+  //       color: 'pink-text-gradient',
+  //     },
+  //   ],
+  //   image: nyeusi,
+  //   repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
+  //   demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+  // },
+];
+
+export { services, technologies, experiences, projects };
